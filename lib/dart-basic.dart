aae5d6fbc5ff942654632951f62f98bc57cc54bc
0x1m4o <chuntdevnx@gmail.com>
2023-02-17 11:07:46 +0700

    Dart Basic, Extends, Mixin

---
+++ lib/dart-basic.dart
@@ -0,0 +1,48 @@
+void main() {
+  var animal1 = new Animal(name: 'Swagcat', age: 10);
+  print(animal1.name);
+  print(animal1.age);
+  animal1.menyusui();
+  animal1.jump();
+
+  var animal2 = new Animal(name: 'SwagDog', age: 15);
+  print(animal2.name);
+  print(animal2.age);
+  animal2.menyusui();
+  animal1.jump();
+
+  var animal3 = new Animal(name: 'SwagBat', age: 17);
+  print(animal3.name);
+  print(animal3.age);
+  animal3.menyusui();
+  animal3.fly();
+}
+
+class Animal extends Mammals with Fly, Jump {
+  String? name;
+  int? age;
+
+  /// Positional Arguments
+  // Animal(this.name, this.age);
+
+  /// Named Arguments
+  Animal({this.name, this.age});
+}
+
+class Mammals {
+  void menyusui() {
+    print("This animal can breastfeed because it is a mamals");
+  }
+}
+
+class Jump {
+  void jump() {
+    print('This animal can Jump');
+  }
+}
+
+class Fly {
+  void fly() {
+    print('This animal can fly');
+  }
+}
