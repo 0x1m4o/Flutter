906262f13706bc8ba664435040c23600abb3bef0
0x1m4o <chuntdevnx@gmail.com>
2023-02-20 21:39:56 +0700

    Image(Boxfit cover, fill, etc. AssetImage/Network Image) Widget

--- lib/main.dart
+++ lib/main.dart
@@ -13,178 +13,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("List Tile"),
+          title: Text("Image"),
         ),
-        body: ListView(children: [
-          ListTile(
-            contentPadding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-            title: Text("Swagcat"),
-            subtitle: Text(
-              "Voluptate occaecat nostrud ullamco quis reprehenderit eu duis consequat culpa. Ad est deserunt aliquip in minim nostrud eu laborum sit proident velit. Veniam adipisicing eu ex ullamco minim labore reprehenderit. Voluptate cupidatat sunt ut occaecat exercitation culpa officia consectetur. Ad ea cillum dolore qui id sint qui laborum. Est ad nostrud pariatur ullamco cupidatat velit qui nisi ea aliqua culpa veniam sint et.",
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            leading: CircleAvatar(),
-            trailing: Text("20:44"),
-          ),
-          Divider(
-            color: Colors.grey,
-          ),
-          ListTile(
-            contentPadding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-            title: Text("Swagcat"),
-            subtitle: Text(
-              "Voluptate occaecat nostrud ullamco quis reprehenderit eu duis consequat culpa. Ad est deserunt aliquip in minim nostrud eu laborum sit proident velit. Veniam adipisicing eu ex ullamco minim labore reprehenderit. Voluptate cupidatat sunt ut occaecat exercitation culpa officia consectetur. Ad ea cillum dolore qui id sint qui laborum. Est ad nostrud pariatur ullamco cupidatat velit qui nisi ea aliqua culpa veniam sint et.",
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            leading: CircleAvatar(),
-            trailing: Text("20:44"),
-          ),
-          Divider(
-            color: Colors.grey,
-          ),
-          ListTile(
-            contentPadding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-            title: Text("Swagcat"),
-            subtitle: Text(
-              "Voluptate occaecat nostrud ullamco quis reprehenderit eu duis consequat culpa. Ad est deserunt aliquip in minim nostrud eu laborum sit proident velit. Veniam adipisicing eu ex ullamco minim labore reprehenderit. Voluptate cupidatat sunt ut occaecat exercitation culpa officia consectetur. Ad ea cillum dolore qui id sint qui laborum. Est ad nostrud pariatur ullamco cupidatat velit qui nisi ea aliqua culpa veniam sint et.",
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            leading: CircleAvatar(),
-            trailing: Text("20:44"),
-          ),
-          Divider(
-            color: Colors.grey,
-          ),
-          ListTile(
-            contentPadding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-            title: Text("Swagcat"),
-            subtitle: Text(
-              "Voluptate occaecat nostrud ullamco quis reprehenderit eu duis consequat culpa. Ad est deserunt aliquip in minim nostrud eu laborum sit proident velit. Veniam adipisicing eu ex ullamco minim labore reprehenderit. Voluptate cupidatat sunt ut occaecat exercitation culpa officia consectetur. Ad ea cillum dolore qui id sint qui laborum. Est ad nostrud pariatur ullamco cupidatat velit qui nisi ea aliqua culpa veniam sint et.",
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            leading: CircleAvatar(),
-            trailing: Text("20:44"),
-          ),
-          Divider(
-            color: Colors.grey,
-          ),
-          ListTile(
-            contentPadding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-            title: Text("Swagcat"),
-            subtitle: Text(
-              "Voluptate occaecat nostrud ullamco quis reprehenderit eu duis consequat culpa. Ad est deserunt aliquip in minim nostrud eu laborum sit proident velit. Veniam adipisicing eu ex ullamco minim labore reprehenderit. Voluptate cupidatat sunt ut occaecat exercitation culpa officia consectetur. Ad ea cillum dolore qui id sint qui laborum. Est ad nostrud pariatur ullamco cupidatat velit qui nisi ea aliqua culpa veniam sint et.",
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            leading: CircleAvatar(),
-            trailing: Text("20:44"),
-          ),
-          Divider(
-            color: Colors.grey,
-          ),
-          ListTile(
-            contentPadding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-            title: Text("Swagcat"),
-            subtitle: Text(
-              "Voluptate occaecat nostrud ullamco quis reprehenderit eu duis consequat culpa. Ad est deserunt aliquip in minim nostrud eu laborum sit proident velit. Veniam adipisicing eu ex ullamco minim labore reprehenderit. Voluptate cupidatat sunt ut occaecat exercitation culpa officia consectetur. Ad ea cillum dolore qui id sint qui laborum. Est ad nostrud pariatur ullamco cupidatat velit qui nisi ea aliqua culpa veniam sint et.",
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            leading: CircleAvatar(),
-            trailing: Text("20:44"),
-          ),
-          Divider(
-            color: Colors.grey,
-          ),
-          ListTile(
-            contentPadding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-            title: Text("Swagcat"),
-            subtitle: Text(
-              "Voluptate occaecat nostrud ullamco quis reprehenderit eu duis consequat culpa. Ad est deserunt aliquip in minim nostrud eu laborum sit proident velit. Veniam adipisicing eu ex ullamco minim labore reprehenderit. Voluptate cupidatat sunt ut occaecat exercitation culpa officia consectetur. Ad ea cillum dolore qui id sint qui laborum. Est ad nostrud pariatur ullamco cupidatat velit qui nisi ea aliqua culpa veniam sint et.",
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            leading: CircleAvatar(),
-            trailing: Text("20:44"),
-          ),
-          Divider(
-            color: Colors.grey,
-          ),
-          ListTile(
-            contentPadding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-            title: Text("Swagcat"),
-            subtitle: Text(
-              "Voluptate occaecat nostrud ullamco quis reprehenderit eu duis consequat culpa. Ad est deserunt aliquip in minim nostrud eu laborum sit proident velit. Veniam adipisicing eu ex ullamco minim labore reprehenderit. Voluptate cupidatat sunt ut occaecat exercitation culpa officia consectetur. Ad ea cillum dolore qui id sint qui laborum. Est ad nostrud pariatur ullamco cupidatat velit qui nisi ea aliqua culpa veniam sint et.",
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            leading: CircleAvatar(),
-            trailing: Text("20:44"),
-          ),
-          Divider(
-            color: Colors.grey,
-          ),
-          ListTile(
-            contentPadding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-            title: Text("Swagcat"),
-            subtitle: Text(
-              "Voluptate occaecat nostrud ullamco quis reprehenderit eu duis consequat culpa. Ad est deserunt aliquip in minim nostrud eu laborum sit proident velit. Veniam adipisicing eu ex ullamco minim labore reprehenderit. Voluptate cupidatat sunt ut occaecat exercitation culpa officia consectetur. Ad ea cillum dolore qui id sint qui laborum. Est ad nostrud pariatur ullamco cupidatat velit qui nisi ea aliqua culpa veniam sint et.",
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            leading: CircleAvatar(),
-            trailing: Text("20:44"),
-          ),
-          Divider(
-            color: Colors.grey,
-          ),
-          ListTile(
-            contentPadding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-            title: Text("Swagcat"),
-            subtitle: Text(
-              "Voluptate occaecat nostrud ullamco quis reprehenderit eu duis consequat culpa. Ad est deserunt aliquip in minim nostrud eu laborum sit proident velit. Veniam adipisicing eu ex ullamco minim labore reprehenderit. Voluptate cupidatat sunt ut occaecat exercitation culpa officia consectetur. Ad ea cillum dolore qui id sint qui laborum. Est ad nostrud pariatur ullamco cupidatat velit qui nisi ea aliqua culpa veniam sint et.",
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            leading: CircleAvatar(),
-            trailing: Text("20:44"),
-          ),
-          Divider(
-            color: Colors.grey,
-          ),
-          ListTile(
-            contentPadding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-            title: Text("Swagcat"),
-            subtitle: Text(
-              "Voluptate occaecat nostrud ullamco quis reprehenderit eu duis consequat culpa. Ad est deserunt aliquip in minim nostrud eu laborum sit proident velit. Veniam adipisicing eu ex ullamco minim labore reprehenderit. Voluptate cupidatat sunt ut occaecat exercitation culpa officia consectetur. Ad ea cillum dolore qui id sint qui laborum. Est ad nostrud pariatur ullamco cupidatat velit qui nisi ea aliqua culpa veniam sint et.",
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            leading: CircleAvatar(),
-            trailing: Text("20:44"),
-          ),
-          Divider(
-            color: Colors.grey,
-          ),
-          ListTile(
-            contentPadding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-            title: Text("Swagcat"),
-            subtitle: Text(
-              "Voluptate occaecat nostrud ullamco quis reprehenderit eu duis consequat culpa. Ad est deserunt aliquip in minim nostrud eu laborum sit proident velit. Veniam adipisicing eu ex ullamco minim labore reprehenderit. Voluptate cupidatat sunt ut occaecat exercitation culpa officia consectetur. Ad ea cillum dolore qui id sint qui laborum. Est ad nostrud pariatur ullamco cupidatat velit qui nisi ea aliqua culpa veniam sint et.",
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            leading: CircleAvatar(),
-            trailing: Text("20:44"),
-          ),
-          Divider(
-            color: Colors.grey,
-          ),
-        ]),
+        body: Center(
+          child: Container(
+              width: 350,
+              height: 500,
+              color: Colors.lightBlue,
+              child:
+                  // Image.asset("images/swagcat1.png",
+                  //     fit:
+                  //         // BoxFit.fill,
+                  //         BoxFit.cover),
+
+                  /// OR
+
+                  Image(
+                      image:
+                          // AssetImage("images/swagcat1.png"),
+                          NetworkImage("https://picsum.photos/200/300"))),
+        ),
       ),
     );
   }
