14f3d4a7db6b631fa65c43e5d7bfaeccd6dd52df
0x1m4o <chuntdevnx@gmail.com>
2023-02-21 12:02:48 +0700

    Mapping List and Singlechildscrollview widget - ID Apps

--- lib/kuldii-flutter/mapping_list-ID_Apps.dart
+++ lib/kuldii-flutter/mapping_list-ID_Apps.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:learning_flutter/kuldii-flutter/extract_and_faker.dart';
 
 main() {
   runApp(MyApp());
@@ -12,133 +11,82 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final List<Map<String, dynamic>> myData = [
       {
-        "Name": "Swagcat",
-        "Age": 25,
-        "profilePict": "images/swagcat1.png",
-        "favColor": [
-          "black",
-          "green",
-          "blue",
-          "black",
-          "green",
-          "blue",
-          "black",
-          "green",
-          "blue",
+        "name": "Swagcat",
+        "age": 12,
+        "profilepic": "swagcat1.png",
+        "favFood": [
+          "Pizza",
+          "Noodles",
+          "Briyani",
+          "Rice",
+          "Pizza",
+          "Noodles",
+          "Briyani",
+          "Rice",
+          "Pizza",
+          "Noodles",
+          "Briyani",
+          "Rice"
         ]
       },
       {
-        "Name": "SwagDog",
-        "Age": 35,
-        "profilePict": "images/swagcat2.png",
-        "favColor": ["red", "pink", "lightblue"]
+        "name": "SwagDog",
+        "age": 12,
+        "profilepic": "swagcat2.png",
+        "favFood": ["Pizza", "Noodles", "Briyani", "Rice"]
       },
       {
-        "Name": "SwagDog",
-        "Age": 35,
-        "profilePict": "images/swagcat3.png",
-        "favColor": ["red", "pink", "lightblue"]
+        "name": "SwagBat",
+        "age": 12,
+        "profilepic": "swagcat3.png",
+        "favFood": ["Pizza", "Noodles", "Briyani", "Rice"]
       },
-      {
-        "Name": "SwagDog",
-        "Age": 35,
-        "profilePict": "images/swagcat4.png",
-        "favColor": ["red", "pink", "lightblue"]
-      },
-      {
-        "Name": "SwagDog",
-        "Age": 35,
-        "profilePict": "images/swagcat5.png",
-        "favColor": ["red", "pink", "lightblue"]
-      },
-      {
-        "Name": "SwagDog",
-        "Age": 35,
-        "profilePict": "images/swagcat3.png",
-        "favColor": ["red", "pink", "lightblue"]
-      }
     ];
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Mapping List - ID Apps'),
-          ),
-          body: ListView(
-            children: myData.map((data) {
-              List myfavColor = data["favColor"];
-              return Card(
-                margin: EdgeInsets.all(10),
-                color: Colors.grey.withOpacity(0.3),
-                child: Container(
-                  margin: EdgeInsets.all(10),
-                  child: Column(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Mapping List'),
+      ),
+      body: ListView(
+        children: myData.map((data) {
+          List myFood = data["favFood"];
+          return Card(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage('images/${data['profilepic']}'),
+                  ),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Row(
-                        children: [
-                          CircleAvatar(
-                            backgroundImage:
-                                AssetImage("${data["profilePict"]}"),
-                          ),
-                          SizedBox(
-                            width: 12,
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Name : ${data["Name"]}'),
-                              Text(
-                                'Age : ${data["Age"]}',
-                                style: TextStyle(fontSize: 12),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                      SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: myfavColor.map((color) {
-                          return Container(
-                            child: Text(color),
-                            color: Colors.lightBlue,
-                            margin: EdgeInsets.all(8),
-                            padding: EdgeInsets.all(10),
-                          );
-                        }).toList()),
-                      )
+                      Text('Name : ${data["name"]}'),
+                      Text('Age : ${data["age"]}')
                     ],
                   ),
+                ],
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: myFood.map((food) {
+                    return Container(
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.all(10),
+                      color: Colors.lightBlue,
+                      child: Text(food),
+                    );
+                  }).toList(),
                 ),
-              );
-            }).toList(),
-          )
-          // Column(
-          //   children: [
-          //     Row(
-          //       children: [
-          //         CircleAvatar(),
-          //         Column(
-          //           children: [Text('Name'), Text('Age')],
-          //         )
-          //       ],
-          //     ),
-          //     Row(
-          //       children: [
-          //         Container(
-          //           child: Text('Hitam'),
-          //         ),
-          //         Container(
-          //           child: Text('Kuning'),
-          //         ),
-          //         Container(
-          //           child: Text('Hijau'),
-          //         ),
-          //       ],
-          //     )
-          //   ],
-          // ),
-          ),
-    );
+              )
+            ]),
+          );
+        }).toList(),
+      ),
+    ));
   }
 }
